bd4fcdc00785d3ed06a05573686619a1cb389061
Berlin <3q.berlin@gmail.com>
2024-10-06 10:35:38 +0800

    build test and testing

---
+++ test.js
@@ -0,0 +1,9 @@
+var expect = require("expect.js");
+
+describe("單元測試", function () {
+  describe("test hello", function () {
+    it("hello", function () {
+      expect(1).to.equal(1);
+    });
+  });
+});
