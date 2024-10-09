f02f73f7737046b0c4ceb0550434754e954a8e67
Berlin <3q.berlin@gmail.com>
2024-10-09 21:46:13 +0800

    install plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,5 @@
 export default {
-  extends: [
-    'eslint:recommended', // 使用 ESLint 推薦的基礎規則
-  ],
+  extends: ['eslint:recommended', 'prettier'],
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': 'error',
